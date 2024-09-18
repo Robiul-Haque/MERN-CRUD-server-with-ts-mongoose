724ac0bf0b7df8ea56297d9a9a6760521d11ab08
Robiul Haque <robiulcoc420@gmail.com>
2024-09-19 01:24:46 +0600

    add new function for recive data inside parameter

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,20 +6,24 @@ import catchAsync from "../utils/catchAsync";
 import AppError from "../errors/appError";
 import httpStatus from "http-status";
 
-const auth = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
-    }
+const auth = (role: string) => {
+    // console.log(role);
 
-    const decoded = jwt.verify(token, config.jwt_access_key as string) as JwtPayload;
-    const { email } = decoded;
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.headers.authorization;
+        if (!token) {
+            throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
+        }
 
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "User not found");
-    }
-    next();
-});
+        const decoded = jwt.verify(token, config.jwt_access_key as string) as JwtPayload;
+        const { email } = decoded;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AppError(httpStatus.NOT_FOUND, "User not found");
+        }
+        next();
+    });
+}
 
 export default auth;
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -7,7 +7,8 @@ const router = express.Router();
 
 // API endpoint for user
 router.post("/signup", uploadImg.single('image'), userController.signUp);
-router.get("/get-single-user/:email", auth, userController.getSingleUser);
-router.put("/user-data-update/:email", auth, uploadImg.single('image'), userController.updateSingleUser);
+router.get("/get-single-user/:email", auth("admin"), userController.getSingleUser);
+
+router.put("/user-data-update/:email", auth("user"), uploadImg.single('image'), userController.updateSingleUser);
 
 export const userRoutes = router;
