7cd67395cc4a0d683a15d6fc947ade37415c7255
Robiul Haque <robiulcoc420@gmail.com>
2024-09-23 10:25:28 +0600

    change bodyParser.ts code

--- src/middleware/bodyParser.ts
+++ src/middleware/bodyParser.ts
@@ -3,9 +3,7 @@ import AppError from "../errors/appError";
 import catchAsync from "../utils/catchAsync";
 
 const bodyParser = catchAsync((req: Request, res: Response, next: NextFunction) => {
-    if (!req.body.data) {
-        throw new AppError(400, 'Please provide data in the body under data key');
-    }
+    if (!req.body.data) throw new AppError(400, 'Please provide data in the body under data key');
     req.body = JSON.parse(req.body.data);
 
     next();
