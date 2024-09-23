f51bea1b2edf7ed9866f7fa38bb87154062cbf6c
Robiul Haque <robiulcoc420@gmail.com>
2024-09-23 10:23:51 +0600

    change auth.ts code

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -11,17 +11,12 @@ const auth = (role: string) => {
 
     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
         const token = req.headers.authorization;
-        if (!token) {
-            throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
-        }
+        if (!token) throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
 
-        const decoded = jwt.verify(token, config.jwt_access_key as string) as JwtPayload;
-        const { email } = decoded;
+        const { email } = jwt.verify(token, config.jwt_access_key as string) as JwtPayload;
 
         const user = await User.findOne({ email });
-        if (!user) {
-            throw new AppError(httpStatus.NOT_FOUND, "User not found");
-        }
+        if (!user) throw new AppError(httpStatus.NOT_FOUND, "User not found");
 
         next();
     });
