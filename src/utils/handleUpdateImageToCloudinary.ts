1e0d10384525bd91297e6e5d17f7429f3d150b81
Robiul-Haque <robiulcoc420@gmail.com>
2024-12-30 01:54:39 +0600

    make update image on cloudinary file

---
+++ src/utils/handleUpdateImageToCloudinary.ts
@@ -0,0 +1,33 @@
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
+import AppError from '../errors/appError';
+
+// Function to delete an image from Cloudinary
+const deleteCloudinaryImage = async (public_id: string) => {
+    cloudinary.uploader
+        .destroy(public_id)
+        .then()
+        .catch(err => new AppError(500, `Failed to upload new image: ${err?.message}`));
+};
+
+// Function to upload a new image to Cloudinary and delete old image locally and on Cloudinary
+export const updateImageOnCloudinary = async (newImgName: string, newImgPath: string, existingImgPublicId: string) => {
+    // Delete the existing image from Cloudinary
+    if (existingImgPublicId) await deleteCloudinaryImage(existingImgPublicId);
+
+    // Upload the new image to Cloudinary
+    const result: UploadApiResponse = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(
+            newImgPath,
+            {
+                public_id: newImgName, // Set the name of the uploaded file
+                folder: "Rik-student-portfolio", // Specify the folder in Cloudinary
+            },
+            (err, result) => {
+                if (err) reject(new AppError(500, `Failed to upload new image: ${err.message}`));
+                resolve(result as UploadApiResponse);
+            },
+        );
+    });
+
+    return result;
+};
