11dae0f30b19841d5c00f6e527a5ed916e342a2e
Robiul-Haque <robiulcoc420@gmail.com>
2024-12-30 01:47:38 +0600

    create upload image on cloudinary

---
+++ src/utils/handleImageToCloudinary.ts
@@ -0,0 +1,25 @@
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
+import config from "../config";
+
+// Cloudinary configration
+cloudinary.config({
+    cloud_name: config.cloud_name,
+    api_key: config.api_key,
+    api_secret: config.api_key,
+});
+
+export const uploadImageToCloudinary = async (imgName: string, imgPath: string) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(
+            imgPath,
+            {
+                public_id: imgName, // Set the name of the uploaded file
+                folder: "Mern-crud", // Specify the folder in Cloudinary
+            },
+            (err, result) => {
+                if (err) reject(err);
+                resolve(result as UploadApiResponse);
+            }
+        );
+    });
+}
